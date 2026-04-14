6c763a16f5dd5828bf49433630f7a7fdf891a838
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-04-15 00:00:45 +0530

    Added the practice code file of Swing

---
+++ AWT-gui/Swinggui.java
@@ -0,0 +1,12 @@
+// A practice code file learning the concepts of Swing
+import javax.swing.*;
+public class Swinggui {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Registration Form");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+    }
+}
